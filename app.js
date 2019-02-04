0f3832d86fa0129aa45236b8d500d8440449021a
Arvind Chembarpu <achembarpu@google.com>
2019-02-04 18:07:26 +0100

    Add files for open sourcing

---
+++ app.js
@@ -0,0 +1,374 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const Hapi = require('hapi');
+const {GoogleAuth} = require('google-auth-library');
+const {BigQuery} = require('@google-cloud/bigquery');
+const {DateTime} = require('luxon');
+const {Readable} = require('stream');
+
+const {createKey, deleteKey} = require('./auth.js');
+
+
+// Configurable values
+const LOOKBACK_DAYS = 7;
+const DATE_FIELD = 'date';
+
+
+const IAM_PAUSE_MS = 1000;
+
+const REPORTING_BASE_URL = `https://www.googleapis.com/dfareporting/v3.2`;
+const REPORTING_SCOPE = ['https://www.googleapis.com/auth/dfareporting'];
+
+const DFA_NAME_PATTERN = /^dfa:(?<name>.*)/;
+const REPORT_AVAIL_PATTERN = /REPORT_AVAILABLE/;
+const FIELDS_PATTERN = /\nReport Fields\n(?<fields>.*)\n/;
+const ENDING_PATTERN = /\nGrand Total:/;
+const DATE_FORMAT = 'yyyy-MM-dd';
+
+
+async function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+function log(thing) {
+  console.log(thing);
+}
+
+function info(msg) {
+  console.log(`INFO: ${msg}`);
+}
+
+function error(err) {
+  console.error(`ERROR: ${err.message || err}`);
+}
+
+async function failure(h, err) {
+  error(err);
+  return h.response({message: 'failure'}).code(500);
+}
+
+async function success(h, msg) {
+  info(msg);
+  return h.response({message: 'success'}).code(200);
+}
+
+/**
+ * Extracts valid fieldnames for dimensions & metrics from their DFA API names.
+ *
+ * @param {object} data DFA API response for the report
+ * @return {array} camelCased fieldnames
+ */
+function getFieldnames(data) {
+  const criteriaType = data.type
+      .toLowerCase()
+      .replace( /_(.)/g, (c) => c.toUpperCase())
+      .replace(/_/g, '');
+  const criteria = data[`${criteriaType}Criteria`] || data.criteria;
+  const dimensions = criteria.dimensions.map((dimension) => (dimension.name));
+  const metrics = criteria.metricNames;
+  const dfaNames = dimensions.concat(metrics);
+  const names = dfaNames.map((name) => DFA_NAME_PATTERN.exec(name).groups.name);
+  return names;
+}
+
+/**
+ * Constructs fields from names with associated datatypes.
+ *
+ * @param {array!} names Fieldnames
+ * @return {array!} Fields with types
+ */
+function getFields(names) {
+  const fields = names.map((name) => {
+    let type = 'STRING';
+    if (name === DATE_FIELD) {
+      type = 'DATE';
+    }
+    return {name, type};
+  });
+  return fields;
+}
+
+/**
+ * Compares BigQuery schema for matching field names and types.
+ *
+ * @param {object!} actual Schema to compare against
+ * @param {object!} test Schema to check validity of
+ * @return {boolean} Whether schema matches
+ */
+function compareSchema(actual, test) {
+  if (actual.length !== test.length) {
+    return false;
+  }
+  for (const i in test) {
+    if (!test[i]
+        || actual[i].name !== test[i].name
+        || actual[i].type !== test[i].type) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * Generates lookback dates relative to the provided one.
+ *
+ * @param {DateTime!} fromDate Starting date for lookback
+ * @param {number} numDays Number of days to lookback
+ * @param {string=} dateFormat Format string for date
+ * @return {array!} Lookback date strings
+ */
+function getLookbackDates(fromDate, numDays, dateFormat = DATE_FORMAT) {
+  const lookbackDates = {};
+  for (let days = 1; days <= numDays; days++) {
+    const date = fromDate.minus({days}).toFormat(dateFormat);
+    lookbackDates[date] = null;
+  }
+  return lookbackDates;
+}
+
+
+async function main(req, h) {
+  // handler function aliases, bound to the hapi `h` handler
+  const reject = (err) => failure(h, err);
+  const resolve = (msg) => success(h, msg);
+
+  let projectId, emailId, keyId;
+
+  try {
+    const datasetId = req.params.datasetId;
+    const reportId = req.params.reportId;
+
+    projectId = process.env.GOOGLE_CLOUD_PROJECT;
+    if (!projectId) {
+      throw Error('Provide Google Cloud Project ID');
+    }
+
+    if (!req.payload) {
+      throw Error('Provide DCM Profile ID and Account Email ID.');
+    }
+
+    const profileId = req.payload.profileId;
+    if (!profileId) {
+      throw Error('Provide DCM Profile ID');
+    }
+
+    emailId = req.payload.emailId;
+    if (!emailId) {
+      throw Error('Provide Account Email ID');
+    }
+
+    const reportUrl = `${REPORTING_BASE_URL}` +
+      `/userprofiles/${profileId}` +
+      `/reports/${reportId}`;
+
+    info(`Connector started for Report ${reportId} & Dataset ${datasetId}.`);
+
+    info(`Creating a new IAM key for ${emailId} on project ${projectId}.`);
+    const credentials = await createKey(projectId, emailId);
+    keyId = credentials.private_key_id;
+
+    info(`Waiting for IAM key ${keyId} to be active on GCP.`);
+    await sleep(IAM_PAUSE_MS);
+
+    info('Initializing the DCM client.');
+    const auth = new GoogleAuth();
+    const dcm = await auth.getClient({scopes: REPORTING_SCOPE, credentials});
+
+    info(`Checking for existence of Report ${reportId}`);
+    const {data: checkData} = await dcm.request({url: reportUrl});
+    if (!checkData) {
+      throw Error('Report not found.');
+    }
+
+    info('Evaluating Report metadata.');
+    const reportName = checkData.name;
+    const names = getFieldnames(checkData);
+    const fields = getFields(names);
+    info(`Report Name: ${reportName}`);
+    info(`Report Fields:`);
+    log(fields);
+
+    info('Initializing the BQ client.');
+    const bq = await new BigQuery({projectId, credentials});
+
+    info(`Checking for existence of Dataset ${datasetId}`);
+    const dataset = bq.dataset(datasetId);
+    const [datasetExists] = await dataset.exists();
+    if (!datasetExists) {
+      throw Error('Dataset not found.');
+    }
+
+    const tableName = `${reportName}`;
+    info(`Checking for existence of Table ${tableName}`);
+    const table = dataset.table(tableName);
+    const [tableExists] = await table.exists();
+    if (!tableExists) {
+      error('Table not found.');
+      info(`Creating BQ Table ${datasetId}.${tableName}.`);
+      await table.create({schema: {fields}});
+    }
+
+    info('Fetching BQ table metadata for verification.');
+    const [{schema: {fields: actualFields}}] = await table.getMetadata();
+    const schemaMatches = compareSchema(actualFields, fields);
+    if (!schemaMatches) {
+      throw Error('Schema mismatch encountered.');
+    }
+
+    info('Calculating lookback days.');
+    const today = DateTime.utc();
+    const lookbackDates = getLookbackDates(today, LOOKBACK_DAYS);
+    info(`Lookback dates: ${Object.keys(lookbackDates)}`);
+
+    info('Checking ingested dates.');
+    const [rows] = await bq.query(`
+      SELECT DISTINCT ${DATE_FIELD}
+      FROM \`${projectId}:${datasetId}.${tableName}\`
+      ORDER BY ${DATE_FIELD} DESC
+      LIMIT ${LOOKBACK_DAYS}
+    `);
+    const ingestedDates = rows.map((row) => row[DATE_FIELD].value);
+    info(`Ingested dates: ${ingestedDates}`);
+
+    info('Calculating required dates.');
+    for (const date of ingestedDates) {
+      if (date in lookbackDates) {
+        delete lookbackDates[date]; // remove ingested dates
+      }
+    }
+    info(`Required dates: ${Object.keys(lookbackDates)}`);
+
+    info('Enumerating DCM reports.');
+    const reportFiles = {};
+    let nextPageToken = null;
+    do {
+      const enumUrl = `${reportUrl}/files`;
+      const enumParams = {
+        maxResults: 10,
+        sortField: 'LAST_MODIFIED_TIME',
+        sortOrder: 'DESCENDING',
+        nextPageToken,
+      };
+
+      const {data} = await dcm.request({url: enumUrl, params: enumParams});
+      if (!data) {
+        throw Error('Report files not found.');
+      }
+      nextPageToken = data.nextPageToken;
+
+      let latestDate;
+      for (const item of data.items) {
+        if (item.status.match(REPORT_AVAIL_PATTERN)) {
+          const reportDate = item.dateRange.endDate;
+          if (reportDate in lookbackDates) {
+            reportFiles[item.id] = item;
+            delete lookbackDates[reportDate]; // remove found dates
+          }
+          latestDate = DateTime.fromFormat(reportDate, DATE_FORMAT);
+        }
+      }
+
+      if ((today.diff(latestDate).as('days') > LOOKBACK_DAYS) // exceeded window
+          || lookbackDates.length === 0) { // no required dates remaining
+        break;
+      }
+    } while (nextPageToken);
+
+    info(`Unresolved dates: ${Object.keys(lookbackDates)}`);
+    if (!Object.entries(reportFiles).length) {
+      return resolve('No reports to ingest.');
+    }
+
+    info('Ingesting reports.');
+    for (const [fileId, data] of Object.entries(reportFiles)) {
+      info(`Fetching report file ${fileId} for ${data.dateRange.endDate}.`);
+      const fileUrl = data.urls.apiUrl;
+      const {data: file} = await dcm.request({url: fileUrl});
+      if (!file) {
+        error(`${fileId}: Report file is unavailable.`);
+        continue;
+      }
+
+      const fieldsResult = FIELDS_PATTERN.exec(file);
+      if (!fieldsResult) {
+        error(`${fileId}: Fieldnames not found.`);
+        continue;
+      }
+
+      const endingResult = ENDING_PATTERN.exec(file);
+      if (!endingResult) {
+        error(`${fileId}: File ending not found.`);
+        continue;
+      }
+
+      const content = file.slice(
+          fieldsResult.index + fieldsResult[0].length, // end of fields line
+          endingResult.index + 1 // beginning of ending line
+      );
+      const csv = new Readable();
+      csv.push(content);
+      csv.push(null);
+
+      info('Uploading data to BQ table.');
+      try {
+        await new Promise((_resolve, _reject) => (
+          csv.pipe(table.createWriteStream())
+              .on('error', _reject)
+              .on('complete', _resolve)
+        ));
+      } catch (err) {
+        error(`${fileId}: ${err.message}`);
+        continue;
+      }
+    }
+    return resolve('Reports ingested.');
+  } catch (err) {
+    return reject(err);
+  } finally {
+    if (projectId && emailId && keyId) {
+      try {
+        info(`Deleting used IAM key ${keyId}.`);
+        await deleteKey(projectId, emailId, keyId);
+      } catch (err) {
+        error(err);
+      }
+    }
+  }
+}
+
+
+const server = Hapi.server({
+  port: process.env.PORT || 8080,
+  host: '0.0.0.0',
+});
+
+server.route({
+  path: '/{reportId}/{datasetId}',
+  method: 'POST',
+  handler: main,
+});
+
+(async function init() {
+  try {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+  } catch (err) {
+    console.log(`Server crashed with: ${err.message}`);
+  }
+}());
---
+++ auth.js
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const {auth} = require('google-auth-library');
+
+const CLOUD_PLATFORM_SCOPE = ['https://www.googleapis.com/auth/cloud-platform'];
+const IAM_BASE_URL = 'https://content-iam.googleapis.com/v1';
+
+
+/**
+ * Creates a key for the service account. This method uses the default
+ * app engine service account to create the keys, so make sure it
+ * has permissions:
+ * - iam.serviceAccountKeys.create
+ *
+ * @param {string} projectId Cloud project ID
+ * @param {string} serviceAccount Service account email address
+ * @return {object!} Key credentials
+ */
+async function createKey(projectId, serviceAccount) {
+  if (!projectId) {
+    throw Error('Provide a valid GCP project ID.');
+  }
+  if (!serviceAccount) {
+    throw Error('Provide a service account for key creation.');
+  }
+
+  const client = await auth.getClient({scopes: CLOUD_PLATFORM_SCOPE});
+
+  const url = `${IAM_BASE_URL}` +
+    `/projects/${projectId}` +
+    `/serviceAccounts/${serviceAccount}` +
+    `/keys`;
+  const method = 'POST';
+  const params = {
+    privateKeyType: 'TYPE_GOOGLE_CREDENTIALS_FILE',
+    keyAlgorithm: 'KEY_ALG_RSA_2048',
+  };
+
+  const response = await client.request({url, method, params});
+
+  if (!response || !response.data) {
+    throw Error('No response from the default client.');
+  }
+
+  const keyBuffer = Buffer.from(response.data.privateKeyData, 'base64');
+  return JSON.parse(keyBuffer);
+}
+
+/**
+ * deleteKey removes a key from the service account keys.
+ * This method uses the default app engine service account to create the keys,
+ * so make sure it has permissions:
+ * - iam.serviceAccountKeys.delete
+ *
+ * @param {string} projectId Cloud project ID
+ * @param {string} serviceAccount Service account email address
+ * @param {string} privateKeyId Private key ID to delete
+ * @return {Promise!} Deletion request
+ */
+async function deleteKey(projectId, serviceAccount, privateKeyId) {
+  if (!projectId) {
+    throw Error('Provide a valid GCP project ID.');
+  }
+  if (!serviceAccount) {
+    throw Error('Provide a service account for key deletion.');
+  }
+  if (!privateKeyId) {
+    throw Error('Provide a key ID to delete.');
+  }
+
+  const client = await auth.getClient({scopes: CLOUD_PLATFORM_SCOPE});
+
+  const url = `${IAM_BASE_URL}` +
+    `/projects/${projectId}` +
+    `/serviceAccounts/${serviceAccount}` +
+    `/keys/${privateKeyId}`;
+  const method = 'DELETE';
+
+  return client.request({url, method});
+}
+
+exports.deleteKey = deleteKey;
+exports.createKey = createKey;
